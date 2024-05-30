fe1a799a6c7825cd670def244c630fe7ab725ed7
kocmetehan <metehankoc51@gmail.com>
2024-05-30 16:02:14 +0300

    Program to convert string to morse is written

---
+++ main.py
@@ -0,0 +1,20 @@
+CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.' 
+        }
+
+print("\n**************************\nWelcome to the Text to Morse Code Converter\nThis program is used to convert given input to morse code and play corresponding sound\n**************************\n")
+plaintext = input("Please enter the text which you want to convert to morse code: ")
+morse_code = '/'.join([CODE[char] for char in plaintext.upper()])
+print(morse_code)
